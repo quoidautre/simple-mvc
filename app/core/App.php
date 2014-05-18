49e10224f765f9232b938994f905d2e39c3791c8
kalekip1 <kalekip1@gmail.com>
2014-05-19 00:26:19 +0200

    Added backward compatibility for PHP 5.4. Added root constants. Changed some styles.

--- app/core/App.php
+++ app/core/App.php
@@ -15,24 +15,24 @@ class App
      * @var string
      */
     protected $controller = 'home';
-
+    
     /**
      * Stores the method from the split URL
      * @var string
      */
     protected $method = 'index';
-
+    
     /**
      * Stores the parameters from the split URL
      * @var array
      */
     protected $params = array();
-
+    
     public function __construct()
     {
         // Get broken up URL
         $url = $this->parseUrl();
-
+        
         // Does the requested controller exist?
         // If so, set it and unset from URL array
         if(file_exists('../app/controllers/' . $url[0] . '.php'))
@@ -40,11 +40,11 @@ class App
             $this->controller = $url[0];
             unset($url[0]);
         }
-
+        
         require_once '../app/controllers/' . ucfirst($this->controller) . '.php';
-
+        
         $this->controller = new $this->controller;
-
+        
         // Has a second parameter been passed?
         // If so, it might be the requested method
         if(isset($url[1]))
@@ -52,19 +52,19 @@ class App
             if(method_exists($this->controller, $url[1]))
             {
                 $this->method = $url[1];
-
+                
                 unset($url[1]);
             }
         }
-
+        
         // Set parameters to either the array values or an empty array
         $this->params = $url ? array_values($url) : array();
-
+        
         // Call the chosen method on the chosen controller, passing
         // in the parameters array (or empty array if above was false)
-        call_user_func_array([$this->controller, $this->method], $this->params);
+        call_user_func_array(array($this->controller, $this->method), $this->params);
     }
-
+    
     /**
      * Parse the URL for the current request. Effectivly splits it, stores the controller
      * and the method for that controller.
--- app/init.php
+++ app/init.php
@@ -2,3 +2,20 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+
+/*
+| Root path for inclusion.
+*/
+define('INC_ROOT', dirname(__DIR__));
+
+/*
+| Root path for assets
+*/
+define('ASSET_ROOT',
+    'http://'.$_SERVER['HTTP_HOST'].
+    str_replace(
+        $_SERVER['DOCUMENT_ROOT'],
+        '',
+        str_replace('\\', '/', dirname(__DIR__)).'/public'
+    )
+);
