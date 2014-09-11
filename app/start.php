a9da4cdc82f090c64685cef3c2b21c8d2e3fc177
Alex Garrett <alex@phpacademy.org>
2014-09-11 11:28:15 +0100

    Comment tidy

--- app/start.php
+++ app/start.php
@@ -1,8 +1,6 @@
 <?php
 
-/*
- * Root path for inclusion.
- */
+// Root path for inclusion.
 define('INC_ROOT', dirname(__DIR__));
 
 // Require composer autoloader
@@ -12,9 +10,7 @@ require_once INC_ROOT . '/vendor/autoload.php';
 require_once INC_ROOT . '/app/core/App.php';
 require_once INC_ROOT . '/app/core/Controller.php';
 
-/*
- * Root path for assets
- */
+// Root path for assets
 define('ASSET_ROOT',
     'http://'.$_SERVER['HTTP_HOST'].
     str_replace(
