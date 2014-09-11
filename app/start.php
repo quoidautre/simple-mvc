2763faa230ed3a215fb0a03d2e814b1f2fc73e11
Alex Garrett <alex@phpacademy.org>
2014-09-11 11:27:35 +0100

    Require composer autoloader

--- app/start.php
+++ app/start.php
@@ -5,6 +5,10 @@
  */
 define('INC_ROOT', dirname(__DIR__));
 
+// Require composer autoloader
+require_once INC_ROOT . '/vendor/autoload.php';
+
+// Require core files
 require_once INC_ROOT . '/app/core/App.php';
 require_once INC_ROOT . '/app/core/Controller.php';
 
